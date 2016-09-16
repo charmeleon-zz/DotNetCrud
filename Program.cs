1ae0ca0b49d37951776473f3e2e7e27c01328c96
Erick Rivas <erick@simplenight.com>
2016-09-15 20:46:13 -0500

    Install identified dependencies, add .gitignore

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudForm
+{
+    public class DatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=crud.db");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
